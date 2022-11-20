79f135f8be0e698df2c46f5e685d1dd8a4cb9849
Thoi23 <110298348+Thoi23@users.noreply.github.com>
2022-11-20 21:12:00 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -296,7 +296,7 @@ else:
             banner='figlet COOE|lolcat'
             system(banner)
             print("----------Your play time-----------")
-            print("06-20th Nov 2022, 02:00 AM- 08:30 PM")
+            print("06-20th Nov 2022, 02:00 AM- 09:30 PM")
             print("06-14th Oct 2023, 06:00 PM- 06:30 PM")
             print("06-14th Oct 2023, 08:30 PM- 09:00 PM")
             print("Please play on the given time, and ")
@@ -310,7 +310,7 @@ else:
         elif(bhai==night):
             clear()
             print("----------Your play time-----------")
-            print("20th Nov 2022,  08:00 AM- 08:30 PM")
+            print("20th Nov 2022,  08:00 AM- 09:30 PM")
             print("12th Feb 2024, 08:00 PM- 08:30 PM")
             print("13th Feb 2024, 08:00 PM- 08:30 PM")
             print("Please play on the given time, and ")
